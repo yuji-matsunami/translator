183ee444df08bfe9c8802f418f6c535edaec55ad
yuji-matsunami <yuji.sohuto@gmail.com>
2022-05-06 14:46:48 +0900

    add parameter setting

---
+++ main.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pad_sequence
+from typing import List
+from preprocess_data import make_vocab_transform
+from model import DEVICE, Seq2Seq
+
+
+def sequential_transforms(*transforms):
+    def func(txt_input):
+        for transform in transforms:
+            txt_input = transform(txt_input)
+        return txt_input
+    return func
+
+
+def tensor_transform(token_ids: List[int]):
+    return torch.cat((torch.tensor([BOS_IDX]),
+                      torch.tensor(token_ids),
+                      torch.tensor([EOS_IDX])))
+
+
+if __name__ == "__main__":
+    """
+    # ハイパーパラメータの設定
+    """
+    SRC_LANGUAGE = 'de'
+    TGT_LANGUAGE = 'en'
+    UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
+    torch.manual_seed(0)
+    vocab_transform, token_transform = make_vocab_transform(
+        UNK_IDX, src_ln=SRC_LANGUAGE, tgt_ln=TGT_LANGUAGE)
+    SRC_VOCAB_SIZE = len(token_transform[SRC_LANGUAGE])
+    TGT_VOCAB_SIZE = len(token_transform[TGT_LANGUAGE])
+    print(SRC_VOCAB_SIZE, TGT_VOCAB_SIZE)
+    EMB_SIZE = 512
+    NHEAD = 8
+    FFN_HID_DIM = 512
+    BATCH_SIZE = 128
+    NUM_ENCODE_LAYERS = 3
+    NUM_DECODE_LAYERS = 3
+
+    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    transformer = Seq2Seq(NUM_ENCODE_LAYERS, NUM_DECODE_LAYERS, EMB_SIZE, NHEAD,
+                          SRC_VOCAB_SIZE, TGT_VOCAB_SIZE, FFN_HID_DIM)
+
+    # ハイパーパラメータを設定
+    # seed固定しているのでいつも同じになる
+    for p in transformer.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+
+    transformer = transformer.to(DEVICE)
+    loss_fn = nn.CrossEntropyLoss(ignore_index=PAD_IDX)
+    optimizer = torch.optim.Adam(
+        transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
+
+    text_transform = {}
+    for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
+        text_transform[ln] = sequential_transforms(token_transform[ln], # tokenizeする
+                                                   vocab_transform[ln], # 単語IDにする
+                                                   tensor_transform) # BOS,EOSを追加する
+    
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,7 +1,7 @@
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torchtext.datasets import Multi30k
-from typing import Iterable, List
+from typing import Iterable, List, Dict, Any, Tuple
 
 SRC_LANGUAGE = 'de'
 TGT_LANGUAGE = 'en'
@@ -19,6 +19,28 @@ def yield_tokens(data_iter: Iterable, language: str) -> List[str]:
     for data_sample in data_iter:
         yield token_transform[language](data_sample[language_index[language]])
 
+def make_vocab_transform(unk_idx:int, src_ln:str, tgt_ln:str) -> Tuple[Dict, Dict]:
+    """_summary_
+        token_transformを作成
+    Args:
+        src_ln (str): _description_　ソースの言語
+        tgt_rn (str): _description_　ターゲットの言語
+
+    Returns:
+        Dict[str, Any]: _description_ {"de": Vocab(), "en": Vocab()}
+    """
+    special_simbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+
+    for ln in [src_ln, tgt_ln]:
+        train_iter = Multi30k(split='train', language_pair=(src_ln, tgt_ln))
+        vocab_transform[ln] = build_vocab_from_iterator(yield_tokens(train_iter, ln),
+                                                        min_freq=1,
+                                                        specials=special_simbols,
+                                                        special_first=True)
+
+    for ln in [src_ln, tgt_ln]:
+        vocab_transform[ln].set_default_index(unk_idx)
+    return vocab_transform, token_transform
 
 def main():
     UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
@@ -33,6 +55,7 @@ def main():
 
     for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
         vocab_transform[ln].set_default_index(UNK_IDX)
+    print(vocab_transform)
 
 
 
